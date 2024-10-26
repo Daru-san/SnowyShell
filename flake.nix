68081cde0c2c9f4109debe301dc3294a7c870310
Daru <mica.tui@proton.me>
2024-10-26 18:48:04 +0200

    flake: update flake config

--- flake.nix
+++ flake.nix
@@ -14,26 +14,57 @@
       astal,
     }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      genSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      pkgsFor = nixpkgs.legacyPackages;
     in
     {
-      packages.${system}.default = astal.lib.mkLuaPackage {
-        inherit pkgs;
-        src = ./src;
+      devShells = genSystems (
+        system:
+        let
+          pkgs = pkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              cargo
+              rustc
+              pkg-config
+              clippy
+              rustfmt
+            ];
+          };
+        }
+      );
+      packages = genSystems (
+        system:
+        let
+          pkgs = pkgsFor.${system};
+        in
+        {
+          rust-lib = pkgs.callPackage ./rust/package.nix { };
+          default = astal.lib.mkLuaPackage {
+            inherit pkgs;
+            src = ./.;
 
-        extraPackages =
-          [
-            pkgs.dart-sass
-          ]
-          ++ (with astal.packages.${system}; [
-            hyprland
-            network
-            tray
-            wp
-            mpris
-            bluetooth
-          ]);
-      };
+            extraPackages =
+              [
+                pkgs.dart-sass
+                self.packages.${system}.rust-lib
+              ]
+              ++ (with astal.packages.${system}; [
+                hyprland
+                astal3
+                network
+                tray
+                wireplumber
+                mpris
+                bluetooth
+              ]);
+          };
+        }
+      );
     };
 }
