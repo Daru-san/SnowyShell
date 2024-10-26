969cff4b599e60a7a6bf90d82905623a0a7cdfa0
Daru <mica.tui@proton.me>
2024-10-26 22:11:23 +0200

    src/rust: get accurate cpu usage

--- rust/src/cpu/cpu_usage.rs
+++ rust/src/cpu/cpu_usage.rs
@@ -1,24 +1,20 @@
-use std::{thread::sleep, time::Duration};
-use sysinfo::System;
+use sysinfo::{CpuRefreshKind, RefreshKind, System};
 
 pub fn print_decimal() {
-    let mut sys = System::new();
+    let mut sys =
+        System::new_with_specifics(RefreshKind::new().with_cpu(CpuRefreshKind::everything()));
 
-    loop {
-        sys.refresh_cpu_usage();
-        let usage = sys.global_cpu_usage();
-        print!("\r{:.02}", usage / 100.0);
-        sleep(Duration::from_millis(10));
-    }
+    std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+
+    sys.refresh_cpu_usage();
+    print!("{:.02}", sys.global_cpu_usage() / 100.0);
 }
 
 pub fn print_percent() {
-    let mut sys = System::new();
+    let mut sys =
+        System::new_with_specifics(RefreshKind::new().with_cpu(CpuRefreshKind::everything()));
 
-    loop {
-        sys.refresh_cpu_usage();
-        let usage = sys.global_cpu_usage();
-        print!("\r{:.02}%", usage);
-        sleep(Duration::from_millis(10));
-    }
+    std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+    sys.refresh_cpu_usage();
+    println!("{:.02}%", sys.global_cpu_usage());
 }
