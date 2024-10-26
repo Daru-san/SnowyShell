43c056ba345a2343f893181f856b0e2fa467ca19
Daru <mica.tui@proton.me>
2024-10-26 22:11:40 +0200

    src/rust: display memory once

--- rust/src/mem/mem_usage.rs
+++ rust/src/mem/mem_usage.rs
@@ -1,30 +1,21 @@
-use std::{thread::sleep, time::Duration};
 use sysinfo::System;
 
 pub fn print_decimal() {
     let mut sys = System::new();
 
-    loop {
-        sys.refresh_memory();
-        let usage = sys.used_memory();
-        let total = sys.total_memory();
-        let value = (usage as f64) / (total as f64);
-        print!("\r{:.02}", value);
-        sleep(Duration::from_millis(10));
-    }
+    sys.refresh_memory();
+    let usage = sys.used_memory();
+    let total = sys.total_memory();
+    let value = (usage as f64) / (total as f64);
+    print!("\r{:.02}", value);
 }
 
 pub fn print_percent() {
     let mut sys = System::new();
 
-    loop {
-        sys.refresh_memory();
-        let usage = sys.used_memory();
-        let total = sys.total_memory();
-        let value = (usage as f64) / (total as f64);
-        print!("\r{:.02}%", value * 100.0);
-        sleep(Duration::from_millis(10));
-    }
+    sys.refresh_memory();
+    let usage = sys.used_memory();
+    let total = sys.total_memory();
+    let value = (usage as f64) / (total as f64);
+    print!("\r{:.02}%", value * 100.0);
 }
-
-
