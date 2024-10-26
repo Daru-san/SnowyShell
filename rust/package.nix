c790d3afefccdaad18d3b777abab652b3f2474aa
Daru <mica.tui@proton.me>
2024-10-26 22:12:24 +0200

    nix: update rust src package

--- rust/package.nix
+++ rust/package.nix
@@ -1,5 +1,21 @@
-{ rustPlatform }:
+{ rustPlatform, lib }:
+
 rustPlatform.buildRustPackage {
-  pname = "snowy-libs";
+  pname = "snowy-utils";
+
+  version = "0.1.0";
+
   src = ./.;
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+
+  meta = {
+    description = "Utilities I use with my desktop shell";
+    maintainers = [ lib.maintainers.daru-san ];
+    license = lib.licenses.mit;
+    mainPackage = "snowy-libs";
+  };
+
 }
