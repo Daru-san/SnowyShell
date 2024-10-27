b8c183aeaf98d8336a6885321bbddef59ff8de19
Daru <mica.tui@proton.me>
2024-10-27 20:28:13 +0200

    flake: add utils overlay

--- flake.nix
+++ flake.nix
@@ -19,9 +19,21 @@
         "x86_64-linux"
         "aarch64-linux"
       ];
-      pkgsFor = nixpkgs.legacyPackages;
     in
     {
+      overlays.default =
+        self: prev:
+        let
+          lua = prev.lua.override {
+            packageOverrides = luaself: luaprev: {
+              snowy_utils = prev.callPackage ./rust/package.nix { };
+            };
+          };
+        in
+        {
+          luaPackages = lua.pkgs;
+        };
+
       homeManagerModules = {
         default = self.homeManagerModules.snowy-shell;
         snowy-shell = import ./nix/hm-module.nix self;
@@ -30,7 +42,10 @@
       devShells = genSystems (
         system:
         let
-          pkgs = pkgsFor.${system};
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
         in
         {
           default = pkgs.mkShell {
@@ -40,7 +55,11 @@
               pkg-config
               clippy
               rustfmt
-              self.packages.${system}.snowy-utils
+              (lua.withPackages (ps: [ luaPackages.snowy_utils ]))
+              luarocks
+              luarocks-nix
+              luaPackages.luarocks-build-rust-mlua
+              luaPackages.snowy_utils
             ];
           };
         }
@@ -48,11 +67,13 @@
       packages = genSystems (
         system:
         let
-          pkgs = pkgsFor.${system};
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
         in
         {
           default = self.packages.${system}.snowy-shell;
-          snowy-utils = pkgs.callPackage ./rust/package.nix { };
           snowy-shell = pkgs.callPackage ./nix/package.nix {
             inherit
               self
