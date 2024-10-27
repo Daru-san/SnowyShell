bf4d70ada75ce2335e911fe91cc2428241558097
Daru <mica.tui@proton.me>
2024-10-27 19:49:26 +0200

    nix: fix rust package

--- rust/package.nix
+++ rust/package.nix
@@ -3,7 +3,6 @@
   luaPackages,
   rustPlatform,
   cargo,
-  rustc,
 }:
 let
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
@@ -13,22 +12,19 @@ luaPackages.buildLuarocksPackage {
   version = "dev-1";
   src = ./.;
 
+  disabled = luaPackages.luaOlder "5.1";
+
   knownRockSpec = ./snowy_utils-dev-1.rockspec;
 
   propagatedBuildInputs = [
     luaPackages.luarocks-build-rust-mlua
     cargo
-    rustc
+    rustPlatform.cargoSetupHook
   ];
 
   cargoDeps = rustPlatform.importCargoLock {
     lockFile = ./Cargo.lock;
   };
-
-  preCheck = ''
-    mkdir luarocks
-    luarocks $LUAROCKS_EXTRA_ARGS make --tree=luarocks --deps-mode=all
-  '';
 
   meta = {
     inherit (cargoToml.package) description homepage;
