5b1ac65e0b4d8b7ced20b21a9813268a9d298dce
Daru <mica.tui@proton.me>
2025-02-08 17:50:09 +0200

    crates/idle: init

---
+++ crates/idle/package.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  luaPackages,
+  rustPlatform,
+  cargo,
+  pkg-config,
+}:
+let
+  cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+in
+luaPackages.buildLuarocksPackage {
+  pname = cargoToml.package.name;
+  version = "dev-1";
+  src = ./.;
+
+  disabled = luaPackages.luaOlder "5.1";
+
+  knownRockSpec = ./idle-dev-1.rockspec;
+
+  propagatedBuildInputs = [
+    luaPackages.luarocks-build-rust-mlua
+    cargo
+    rustPlatform.cargoSetupHook
+    pkg-config
+  ];
+
+  cargoDeps = rustPlatform.importCargoLock {
+    lockFile = ./Cargo.lock;
+  };
+
+  meta = {
+    inherit (cargoToml.package) description homepage;
+    maintainers = [ lib.maintainers.daru-san ];
+    license = lib.licenses.mit;
+  };
+
+}
