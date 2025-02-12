270959ce4a8aecb86f4e321211f8446b3b518166
Daru <mica.tui@proton.me>
2025-02-12 18:02:56 +0200

    refactor: add gdkmonitor parameter to NotificationCenter window

--- src/lua/NotificationCenter/init.lua
+++ src/lua/NotificationCenter/init.lua
@@ -1,33 +1,34 @@
 local astal = require("astal")
 
-local Variable = astal.Variable
 local Widget = require("astal.gtk3").Widget
 
 local NotificationsList = require('lua.NotificationCenter.Notifications')
 
-local visible = Variable()
+local App = require("astal.gtk3.app")
 
-local Window = function(gdkmonitor)
+---@param gdkmonitor any
+---@return unknown
+return function(gdkmonitor)
 	local Anchor = astal.require("Astal").WindowAnchor
 
 	return Widget.Window({
 		class_name = "NotificationCenter",
-		gdkmonitor = gdkmonitor,
 		name = "NotificationCenter",
+		gdkmonitor = gdkmonitor,
+		setup = function(self)
+			App:add_window(self)
+		end,
 		anchor = Anchor.TOP + Anchor.RIGHT,
 		Widget.Box({
 			vertical = true,
-			visible = visible(),
+			visible = false,
 			Widget.Box({
 				Widget.Label({
 					class_name = "CenterHeader",
 					label = "NotificationCenter",
-					halign = "center"
 				})
 			}),
 			NotificationsList()
 		}),
 	})
 end
-
-return Window
